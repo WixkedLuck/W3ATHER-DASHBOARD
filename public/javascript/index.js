562026f9f0694a873513031ec1dc0097f250b9bb
David Hybner <dah1998@yahoo.com>
2022-10-27 19:24:51 -0400

    Update index.js
    changed the window function to help with going back a page so that the button would reappear

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,7 +3,8 @@ const sharkbtn = document.querySelector('#start');
 sharkbtn.addEventListener('click', function () {
     sharkbtn.classList.add("animate__animated", "animate__hinge");
     let tID = setTimeout(function () {
-        window.location.href = "./public/app.html";
-        window.clearTimeout(tID);		// clear time out.
+        window.clearTimeout(tID);	
+        window.location.assign ("./public/app.html");
+        	// clear time out.
     }, 2000);
 })
