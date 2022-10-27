0f1cf6d9382527e83932dc7ac5f45bfbf9129f70
David Hybner <dah1998@yahoo.com>
2022-10-27 16:00:33 -0400

    rename folders for github
    fixed file names for GitHub

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,7 +3,7 @@ const sharkbtn =document.querySelector('#start');
 sharkbtn.addEventListener('click', function() {
    sharkbtn.classList.add("animate__animated", "animate__hinge"); 
    let tID = setTimeout(function () {
-    window.location.href = "./assets/app.html";
+    window.location.href = "./public/app.html";
     window.clearTimeout(tID);		// clear time out.
 }, 2000);
 })
