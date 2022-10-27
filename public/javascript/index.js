6fd679294475e26541c7ea2d32425a9d16b3b9a4
David Hybner <dah1998@yahoo.com>
2022-10-27 16:05:13 -0400

    rename css folder for GH
    renamed to stylesheets

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,9 @@
-const sharkbtn =document.querySelector('#start');
+const sharkbtn = document.querySelector('#start');
 
-sharkbtn.addEventListener('click', function() {
-   sharkbtn.classList.add("animate__animated", "animate__hinge"); 
-   let tID = setTimeout(function () {
-    window.location.href = "./public/app.html";
-    window.clearTimeout(tID);		// clear time out.
-}, 2000);
+sharkbtn.addEventListener('click', function () {
+    sharkbtn.classList.add("animate__animated", "animate__hinge");
+    let tID = setTimeout(function () {
+        window.location.href = "./public/app.html";
+        window.clearTimeout(tID);		// clear time out.
+    }, 2000);
 })
